0ef8e0cae85e3b0c796445acc389976f06f63241
Joao Justo <joao.justo@roit.com.br>
2020-04-12 00:18:43 -0300

    Fix put method

--- src/app.js
+++ src/app.js
@@ -53,23 +53,20 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const { title, url, techs, likes } = request.body;
+  const { title, url, techs } = request.body;
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (repositoryIndex === -1) {
     return response.status(400).json({ error: "Repository not found" });
-  }
-
-  if (likes) {
-    return response.json({ likes: repositories[repositoryIndex].likes });
   }
 
   const repository = {
     id,
     title,
     url,
-    techs
+    techs,
+    likes: repositories[repositoryIndex].likes,
   }
 
   repositories[repositoryIndex] = repository;
